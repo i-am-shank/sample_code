8778b3cf9a9d4c9582aa287a3c10e78bd69b8a98
i-am-shank <shashanksinha4444@gmail.com>
2025-01-01 12:20:02 +0530

    demo file modified

--- demo.java
+++ demo.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class demo {
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
+      // input output
       int val1 = sc.nextInt();
       System.out.println("var = " + val1);
     } catch(Exception e) {
+      // catching exception
       System.out.println("Exception : " + e);
     }
   }
